2cf0196126e3f74f3f9d92a12eeb135ae86d7abf
GabMGX <gabrielmessias127@gmail.com>
2026-01-18 07:12:41 -0300

    Criado protocolo de conexão e finalizada classe de conexão com o MySQL.

--- src/BarberUp/DAL/mysql_connection.py
+++ src/BarberUp/DAL/mysql_connection.py
@@ -1,15 +1,48 @@
 import mysql.connector
-from mysql.connector import MySQLConnection
 from mysql.connector.pooling import PooledMySQLConnection
-from mysql.connector.abstracts import MySQLConnectionAbstract
-from typing import Optional, Union
+from mysql.connector.abstracts import MySQLConnectionAbstract, MySQLCursorAbstract
+from typing import Union, Tuple, Any, Optional, List
+from .db_connection import DBConnection
 
 
-class MySQL:
-    def __init__(self) -> None:
-        self._connection: Optional[Union[MySQLConnection, PooledMySQLConnection, MySQLConnectionAbstract]] = None
+class MySQLConnection(DBConnection):
+    def __init__(self, user: str, password: str, database: str, host: str = "localhost", port: int = 3306) -> None:
+        self._connection: Union[MySQLConnectionAbstract, 
+                                PooledMySQLConnection] = mysql.connector.connect(user=user, 
+                                                                                password=password, 
+                                                                                database=database, 
+                                                                                host=host, 
+                                                                                port=port)
+    
+    def execute(self, query: str, params: Tuple[Any, ...] = ()) -> None:
+        cursor: MySQLCursorAbstract = self._connection.cursor()
+        try:
+            cursor.execute(query, params)
+            self._connection.commit()
+        finally:
+            cursor.close()
+    
+    def fetchone(self, query: str, params: Tuple[Any, ...] = ()) -> Optional[Tuple[Any, ...]]:
+        cursor: MySQLCursorAbstract = self._connection.cursor(dictionary=False)
+        try:
+            cursor.execute(query, params)
+            row = cursor.fetchone()
+            if row is None:
+                return None
+            assert isinstance(row, tuple)
+            return row
+        finally:
+            cursor.close()
 
-    def connect(self, user: str, password: str, database: str, host: str = "localhost", port: int = 3306) -> Union[MySQLConnection, PooledMySQLConnection, MySQLConnectionAbstract]:
-        conn = mysql.connector.connect(user=user, password=password, database=database, host=host, port=port)
-        self._connection = conn
-        return conn
+    def fetchall(self, query: str, params: Tuple[Any, ...] = ()) -> List[Tuple[Any, ...]]:
+        cursor: MySQLCursorAbstract = self._connection.cursor(dictionary=False)
+        try:
+            cursor.execute(query, params)
+            rows = cursor.fetchall()
+            result: list[Tuple[Any, ...]] = []
+            for row in rows:
+                assert isinstance(row, tuple)
+                result.append(row)
+            return result
+        finally:
+            cursor.close()
