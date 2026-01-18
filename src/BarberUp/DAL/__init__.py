2ec280190e4dcc3a87ccca6ee8846cb0ed0c07d0
GabMGX <gabrielmessias127@gmail.com>
2026-01-18 15:01:04 -0300

    Criado DAL de Barber

--- src/BarberUp/DAL/__init__.py
+++ src/BarberUp/DAL/__init__.py
@@ -2,11 +2,13 @@ from .db_connection import DBConnection
 from .mysql_connection import MySQLConnection
 from .repository import Repository
 from .client_repo import ClientRepo
+from .barber_repo import BarberRepo
 
 
 __all__ = [
     "DBConnection", 
     "MySQLConnection",
     "Repository",
+    "BarberRepo",
     "ClientRepo"
     ]
